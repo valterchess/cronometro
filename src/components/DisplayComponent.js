f7d0aef1754d94854f0052c2362cfaf59331adff
valterchess <valterrekabano7@gmail.com>
2022-01-28 00:13:46 -0300

    corrigindo detalhe

--- src/components/DisplayComponent.js
+++ src/components/DisplayComponent.js
@@ -12,10 +12,10 @@ function DisplayComponent(props) {
 
   return (
     <div >
-      {h()}&nbsp;&nbsp;
+      {h()}&nbsp;:&nbsp;
       <span>{(props.time.m >= 10) ? props.time.m : "0" + props.time.m}</span>&nbsp;:&nbsp;
       <span>{(props.time.s >= 10) ? props.time.s : "0" + props.time.s}</span>&nbsp;:&nbsp;
-      <span>{(props.time.ms >= 10) ? props.time.ms : "0" + props.time.ms}</span>&nbsp;:&nbsp;
+      <span>{(props.time.ms >= 10) ? props.time.ms : "0" + props.time.ms}</span>&nbsp;&nbsp;
     </div>
   );
 }
